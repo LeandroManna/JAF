3d5fe36e6e532bb7af3025b5f1280d45558f5285
Leandro <leandro.a.manna@gmail.com>
2023-04-08 01:51:46 -0300

    Se modifica validar login para que redireccione a la pagina correcta

--- php/validar_login.php
+++ php/validar_login.php
@@ -14,18 +14,12 @@ if (isset($_POST['submit'])){
     mysqli_stmt_execute($stmt);
     $clientes = mysqli_stmt_get_result($stmt);
 
-    /* $stmt1 = mysqli_prepare($conn, "SELECT * FROM body_pump WHERE id = ?");
-    mysqli_stmt_bind_param($stmt1, "s", $username);
-    mysqli_stmt_execute($stmt1);
-    $bodyPumps = mysqli_stmt_get_result($stmt1); */
-
     $stmt2 = mysqli_prepare($conn, "SELECT * FROM administradores WHERE dni = ?");
     mysqli_stmt_bind_param($stmt2, "s", $username);
     mysqli_stmt_execute($stmt2);
     $administradores = mysqli_stmt_get_result($stmt2);
 
     $num_rows = mysqli_num_rows($clientes);
-    /* $num_rows1 = mysqli_num_rows($bodyPumps); */
     $num_rows2 = mysqli_num_rows($administradores);
 
     if ($num_rows > 0) {
@@ -33,31 +27,31 @@ if (isset($_POST['submit'])){
         $cliente = mysqli_fetch_assoc($clientes);
         $nombre_cliente = $cliente['nombre'];
         $detalle_cliente = $cliente['detalle'];
+        $disciplina_cliente = $cliente['disciplina'];
+        
         // Almacenamiento del nombre del cliente en una variable de sesión
         $_SESSION['nombre'] = $nombre_cliente;
         $_SESSION['detalle'] = $detalle_cliente;
-        // Si los datos son válidos, redirigir a la página "clientes.php"
-        header("Location: clientes.php");
+        
+        // Redireccionar según la disciplina
+        if ($disciplina_cliente == "Musculacion") {
+            header("Location: clientes.php");
+        } elseif ($disciplina_cliente == "Especifico") {
+            header("Location: bodyPump.php");
+        } else {
+            echo "Disciplina desconocida";
+        }
         exit;
     } 
-    /* if ($num_rows1 > 0) {
-        // Obtener el nombre del cliente
-        $bodyPump = mysqli_fetch_assoc($bodyPumps);
-        $nombre_bPump = $bodyPump['nombre'];
-        $detalle_bPump = $bodyPump['detalle'];
-        // Almacenamiento del nombre del cliente en una variable de sesión
-        $_SESSION['nombre'] = $nombre_bPump;
-        $_SESSION['detalle'] = $detalle_bPump;
-        // Si los datos son válidos, redirigir a la página "clientes.php"
-        header("Location: bodyPump.php");
-        exit;
-    }  */
+
     if ($num_rows2 > 0) {
         // Obtener el nombre del Administrador
         $administrador = mysqli_fetch_assoc($administradores);
         $nombre_administrador = $administrador['nombre'];
+        
         // Almacenamiento del nombre del Administrador en una variable de sesión
         $_SESSION['nombre'] = $nombre_administrador;
+        
         // Si los datos son válidos, redirigir a la página "admin-clientes.php"
         header("Location: admin-clientes.php");
         exit;
